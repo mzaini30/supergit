37cd6956f274f29f81be90ca861e5934b88b538d
Zen <muhzaini30@gmail.com>
2022-06-11 15:02:51 +0800

    "Sip"

--- dist/commit.js
+++ dist/commit.js
@@ -7,18 +7,20 @@ import {cwd} from 'process'
 const storage = '/tmp/.supergit.txt'
 
 async function main(){
-	const message = question('Commit message: ')
 	const {stdout: add} = await execa('git', ['add', '.'])
+	const message = question('Commit message: ')
 	const {stdout: commit} = await execa('git', ['commit', '-m', `"${message}"`])
-	console.log(commit)
-	if (!existsSync(storage)){
-		writeFileSync(storage, '')
+	if (commit){
+		console.log(commit)
+		if (!existsSync(storage)){
+			writeFileSync(storage, '')
+		}
+		let data = readFileSync(storage).toString()
+		data = data.split('\n').filter(x => x) // become array
+		data.push(cwd())
+		data = [...new Set(data)]
+		data = data.join('\n')
+		writeFileSync(storage, data)
 	}
-	let data = readFileSync(storage).toString()
-	data = data.split('\n').filter(x => x) // become array
-	data.push(cwd())
-	data = [...new Set(data)]
-	data = data.join('\n')
-	writeFileSync(storage, data)
 }
 main()
