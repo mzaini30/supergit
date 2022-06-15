3305a7a7f681f6f129541ce13ee456160e3bc25e
mzaini30 <muhzaini30@gmail.com>
2022-06-16 00:04:17 +0800

    chore: prepare windows

--- src/getStorage.ts
+++ src/getStorage.ts
@@ -4,13 +4,9 @@ export default function (): string {
   let storage: string;
   if (platform() == "linux") {
     storage = "/tmp/.supergit.txt";
-  }
-
-  else if (platform() == 'win32'){
-    storage = '%AppData%\\supergit.txt'
-  }
-
-   else {
+  } else if (platform() == "win32") {
+    storage = `${process.env.APPDATA}\\supergit.txt`;
+  } else {
     storage = "/sdcard/.supergit.txt";
   }
   return storage;
